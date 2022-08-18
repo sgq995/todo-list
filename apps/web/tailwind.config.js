8d2b83db432c9beb4e35e0e4cfe70afe875b8165
Sebastian Gonzalez Quintero <sgq995@gmail.com>
2022-08-17 19:52:20 -0500

    [FIX] TailwindCSS hot-reloading issue solved (#2)

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{tsx,jsx}"],
+
+  content: ["./src/**/*.{tsx,jsx}", "../../packages/ui/**/*.{tsx,jsx}"],
   theme: {
     extend: {},
   },
